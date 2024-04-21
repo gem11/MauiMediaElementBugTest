49ff1c45a8d4604e60a9b38269630112ca5e1bb1
Martin Gernss <martin@gernss.com>
2024-04-21 21:06:13 +0200

    Add project files.

---
+++ MauiMediaElementBugTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiMediaElementBugTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiMediaElementBugTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiMediaElementBugTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
